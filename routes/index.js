bb510379f49a28762ac17a5d8d00fe629a5f9cf1
Peter Park <jhlosin@gmail.com>
2015-11-18 06:11:38 +0900

    basic server work

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.get('/auth', function(req, res, next) {
+  res.render('/auth/auth.html');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.get('/community', function(req, res, next) {
+  res.render('community');
+});
+
+router.get('/search', function(req, res, next) {
+  res.render('search');
+});
+
+
 module.exports = router;
+
+
